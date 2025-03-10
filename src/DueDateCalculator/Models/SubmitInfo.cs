699fb7bf18d6a5806daaf2eaba97c702fb0f6184
Konrad Prohaszka <cc.kprohaszka@gmail.com>
2025-03-10 19:44:43 +0100

    feat: Create SubmitInfo model and DueDateCalculatorService file

---
+++ src/DueDateCalculator/Models/SubmitInfo.cs
@@ -0,0 +1,7 @@
+namespace DueDateCalculator.Models;
+
+public class SubmitInfo
+{
+    public DateTime SubmitDate { get; set; }
+    public int TurnaroundTimeInHours { get; set; }
+}
---
+++ src/DueDateCalculator/Services/DueDateCalculatorService.cs
@@ -0,0 +1,6 @@
+namespace DueDateCalculator.Models;
+
+public class DueDateCalculatorService
+{
+    
+}
